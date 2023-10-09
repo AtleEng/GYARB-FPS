ef0dbadbd16f7bec1f079dfc27961f60a1c7bc45
AtleEng <atle.engelbrektsson@elev.ga.ntig.se>
2023-10-09 11:12:25 +0200

    TargetMovementUppdate

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,11 +6,13 @@ public class Target : MonoBehaviour
 {
     [SerializeField] int hp = 50;
     [SerializeField] float speed = 3.5f;
-    [SerializeField] float length = 10;
+
+    [SerializeField] Vector3[] movePoints;
+    int pointIndex;
     public void TakeDamage(int amountdamage)
     {
         hp -= amountdamage;
-        print(hp);
+        print(hp + " damage dealt to: " + gameObject.name);
         if (hp <= 0)
         {
             Die();
@@ -24,6 +26,13 @@ public class Target : MonoBehaviour
 
     public void Update()
     {
-        transform.position = new Vector3(-length + Mathf.PingPong(Time.time * speed, length * 2), transform.position.y, transform.position.z);
+        //transform.position = new Vector3(-length + Mathf.PingPong(Time.time * speed, length * 2), transform.position.y, transform.position.z);
+        if ((movePoints[pointIndex] - transform.position).magnitude < 0.01f)
+        {
+            pointIndex++;
+            if (pointIndex >= movePoints.Length) { pointIndex = 0; }
+        }
+        transform.position = Vector3.MoveTowards(transform.position, movePoints[pointIndex], Time.deltaTime * speed);
+
     }
 }
