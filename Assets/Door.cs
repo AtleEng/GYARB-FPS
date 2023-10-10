ed7ca693e754b79429002343979ff9043d211be2
Henry Gillinger <henry.gillinger@ga.ntig.se>
2023-10-10 10:26:49 +0200

    123123

---
+++ Assets/Door.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+  
+[SerializeField] List <Target> target = new List<Target>();
+    void Update()
+    {
+        
+    }
+}
